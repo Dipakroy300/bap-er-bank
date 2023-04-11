8a27576ba0b37244fbb5c18c5ec4adb3f2a3c5f4
Dipakroy300 <dipakroy300@gmail.com>
2023-04-12 00:11:00 +0530

    last finishing

--- js/withdraw.js
+++ js/withdraw.js
@@ -9,16 +9,28 @@ const withdrawTotalElement=document.getElementById('withdraw-total');
 const previousWithdrawTotalString=withdrawTotalElement.innerText;
 const previousWithdrawTotal=parseFloat(previousWithdrawTotalString);
 
-const currentWithdrawTotal=previousWithdrawTotal+newWithdrawAmount;
-withdrawTotalElement.innerText=currentWithdrawTotal;
+
 
 const balanceTotalElement=document.getElementById('balance-total');
 const previousBalanceTotalString=balanceTotalElement.innerText;
 const previousBalanceTotal=parseFloat(previousBalanceTotalString);
 
+withdrawField.value='';
+if(isNaN(newWithdrawAmount)){
+    alert('please enter a valid number');
+    return;
+}
+if(newWithdrawAmount>previousBalanceTotal){
+    alert('ato tk nai ');
+    return;
+}
+
+const currentWithdrawTotal=previousWithdrawTotal+newWithdrawAmount;
+withdrawTotalElement.innerText=currentWithdrawTotal;
+
 const newBalanceTotal=previousBalanceTotal-newWithdrawAmount;
 balanceTotalElement.innerText=newBalanceTotal;
 
-withdrawField.value='';
+
 
 })
